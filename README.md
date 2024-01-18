b7c6925e1fbeb3ec51a2c24ea41a99454f9058f0
Boris-Bodin <boris.bodin@gmail.com>
2024-01-18 22:37:57 +0100

    chore(init): Initial import

---
+++ README.md
@@ -0,0 +1,32 @@
+# Nexus Hook
+
+This repository contains the source code for the Nexus Hook, a link-in-bio application.
+
+## Getting Started
+
+### Prerequisites
+
+-   [Node.js](https://nodejs.org/en/) (v18.18.0)
+
+## Coding Features
+
+-   Next.js
+-   TypeScript
+-   ESLint
+-   Prettier
+-   Husky
+-   Tailwind CSS
+-   SASS
+
+## Roadmap
+
+-   Add Jest
+-   Add vercel deployment
+-   Add storybook
+-   Code the app
+-   Add a database
+-   And more ....
+
+## Notes
+
+-   add `"strip-ansi": "6.0.1",` to `package.json` for fix the following issue : https://github.com/eslint/eslint/discussions/17215
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+import {Metadata} from 'next';
+
+import '@/styles/styles.scss';
+
+export const metadata: Metadata = {
+    title: 'Nexus Nook',
+    description: 'Nexus Nook is a link-in-bio service',
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
-        <html lang="en">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <title>Nexus Nook</title>
-        </head>
-        <body>
-        {children}
-        </body>
+        <html lang='en'>
+            <head>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <title>Nexus Nook</title>
+            </head>
+            <body>{children}</body>
         </html>
     );
 }
