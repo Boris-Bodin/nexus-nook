2e0ae698c4cbd4b3fa138a91b9f90236af08876a
Boris-Bodin <boris.bodin@gmail.com>
2024-01-19 14:49:47 +0100

    fix(profil): Fix import

--- src/features/profil/queries/find-profil-by-slug.query.ts
+++ src/features/profil/queries/find-profil-by-slug.query.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import {QueryFunction} from "@tanstack/react-query";
-import {Profil} from "@/features/profil/dto/profil";
+import {QueryFunction} from '@tanstack/react-query';
+import {Profil} from '@/features/profil/dtos/profil';
 
 export default function findProfilBySlugQuery(slug: string): QueryFunction<Profil> {
     return () => axios.get<Profil>('/api/profil/' + slug).then((res) => res.data);
